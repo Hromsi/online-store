fe046254d8d3b61005f948f476d1dc49c1170dbe
Hromsi <110335382+Hromsi@users.noreply.github.com>
2024-08-14 08:12:05 +0300

    feat: implement product fetching, rendering, and pagination
    Implemented functionality to fetch products from an API, render them on the page, and introduced pagination for better usability. This allows users to browse through products more efficiently.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,65 @@
-import Image from "next/image";
+'use client';
+
+import Card from "@/components/Card";
+import { Flex, Grid } from "@chakra-ui/react";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import type { IProduct } from "../../types";
+import Paginator from "@/components/Paginator";
+import Container from "@/components/blocks/Container";
+
+const PAGE_SIZE = 8;
 
 export default function Home() {
-  return (
+	const [products, setProducts] = useState<IProduct[]>([]);
+	const [curPage, setCurPage] = useState(0);
+	const [pagesQuantity, setPagesQuantity] = useState(1);
+
+	const handlePageChange = (page: number) => {
+		setCurPage(page);
+	};
+
+	const fetchProducts = () => {
+	const url = `https://dummyjson.com/products/category/kitchen-accessories?limit=${PAGE_SIZE}&skip=${PAGE_SIZE * curPage}`;
+
+	axios.get(url)
+		.then(function (response) {
+			const { products: dataProducts, total } = response.data;
+			setProducts(dataProducts);
+			setPagesQuantity(Math.ceil(total / PAGE_SIZE));
+			console.log('response :>> ', response.data);
+		})
+	}
+
+	useEffect(() => {
+		fetchProducts();
+	}, [curPage]);
+
+	return (
 	<main>
-		
-    </main>
-  );
+		<Container pt={10} pb={10}>
+			<Grid templateColumns={['repeat(1, 1fr)', 'repeat(2, 1fr)', 'repeat(3, 1fr)', 'repeat(4, 1fr)']} gap="25px" mb="25px">
+				{products.map((product) => (
+					<Card 
+						key={product.id}
+						id={product.id}
+						brand={product.brand}
+						stock={product.stock}
+						description={product.description}
+						images={product.images}
+						thumbnail={product.thumbnail}
+						title={product.title}
+						price={product.price}
+					/>
+				))}
+			</Grid>
+			<Flex justifyContent="center" mb="25px">
+				<Paginator
+					pagesQuantity={pagesQuantity}
+					handlePageChange={handlePageChange}
+				/>
+			</Flex>
+		</Container>
+	</main>
+	);
 }
