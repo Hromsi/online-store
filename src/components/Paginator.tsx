225e56bba9e8f644e83edbfb2b8a63b0d7b6f254
Hromsi <110335382+Hromsi@users.noreply.github.com>
2024-08-14 08:10:18 +0300

    feat: added Paginator component

---
+++ src/components/Paginator.tsx
@@ -0,0 +1,61 @@
+'use client';
+
+import { ButtonProps, Flex } from "@chakra-ui/react";
+import {
+    Paginator as ChaPaginator,
+    Page,
+    Previous,
+    Next,
+    PageGroup,
+    generatePages,
+  } from 'chakra-paginator';
+
+export default function Paginator({ 
+    pagesQuantity, 
+    handlePageChange 
+} : { 
+    pagesQuantity: number;
+    handlePageChange: (newPage: number) => void;
+}) {
+    const baseStyles: ButtonProps = {
+        w: 7,
+        fontSize: "sm"
+    };
+
+    const normalStyles = {
+        ...baseStyles,
+        bg: 'white'
+    };
+    
+
+    const activeStyles = {
+        ...baseStyles,
+        background: 'blue.300'
+    };
+    return (
+        <Flex gap="30px">
+            <ChaPaginator
+                onPageChange={handlePageChange}
+                pagesQuantity={pagesQuantity - 1}
+                normalStyles={normalStyles}
+                activeStyles={activeStyles}
+                baseStyles={baseStyles}
+            >
+            <Previous bg="white">
+                prev
+            </Previous>
+            <PageGroup>
+                {generatePages(pagesQuantity)?.map((page) => (
+                <Page
+                    key={`paginator_page_${page}`}
+                    page={page}
+                />
+                ))}
+            </PageGroup>
+            <Next >
+                next
+            </Next>
+            </ChaPaginator>
+        </Flex>
+    );
+}
